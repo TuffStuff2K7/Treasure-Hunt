3ef26556c8b0b8ba6adf6e18bdf0d81278771fc9
Atul Hegde <76556622+TuffStuff2K7@users.noreply.github.com>
2021-07-23 19:35:38 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O I C T N N F U", 700,190)
+    fill("lightblue")
+    text("Hint: Blocks of code, makes code modular !!", 700,210)
+    
+    fill("white")
+    textSize(15)
+    text("B A T D S A A E", 100,330)
+    fill("lightblue")
+    text("Hint: Stores and sturctures the data !!", 100,350)
+
+}
